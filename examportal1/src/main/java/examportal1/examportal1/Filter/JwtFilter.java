55672dc70fde86210c208abce9c975d6909ccb62
sid <siddharthingle1010@gmail.com>
2023-04-25 15:30:05 +0530

    testing

--- examportal1/src/main/java/examportal1/examportal1/Filter/JwtFilter.java
+++ examportal1/src/main/java/examportal1/examportal1/Filter/JwtFilter.java
@@ -24,7 +24,7 @@ public class JwtFilter extends OncePerRequestFilter {
     public CustomUserDetailsService userService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+//kkklkljklkl
         final String authorizationHeader = request.getHeader("Authorization");
         String username = null;
         String jwt = null;
